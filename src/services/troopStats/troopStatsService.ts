563d76004fd3036027a3f43bb03529f6ea846d95
GiaKhuong32 <khuonghoanggia3202@gmail.com>
2026-06-04 09:06:57 +0700

    fetchdatadashboard

---
+++ src/services/troopStats/troopStatsService.ts
@@ -0,0 +1,44 @@
+import { apiNoPrefix } from "../api";
+
+export interface DonViItem {
+  tenDonVi: string;
+  quanSoTong: number;
+  quanSoHienDien: number;
+  quanSoVang: number;
+  tyLeHienDien: number;
+}
+
+export interface ThongKeQuanSoResult {
+  ngayBaoCao: string;
+  tongQuanSo: number;
+  tongHienDien: number;
+  tongVang: number;
+  tyLeHienDien: number;
+  tyLeVang: number;
+  soSanh: {
+    homQua: { tangGiam: number; phanTram: number };
+    tuanTruoc: { tangGiam: number; phanTram: number };
+    thangTruoc: { tangGiam: number; phanTram: number };
+  };
+  donViTieuBieu: {
+    hienDienCaoNhat: { ten: string; tyLe: number };
+    vangCaoNhat: { ten: string; tyLe: number };
+  };
+  danhSachDonVi: DonViItem[];
+}
+
+export interface ThongKeQuanSoResponse {
+  success: boolean;
+  code: number;
+  message: string;
+  Result: ThongKeQuanSoResult;
+}
+
+export const troopStatsService = {
+  getThongKe: async (ngayBaoCao: string): Promise<ThongKeQuanSoResult> => {
+    const response = await apiNoPrefix.get<ThongKeQuanSoResponse>("/thong-ke", {
+      params: { ngayBaoCao },
+    });
+    return response.data.Result;
+  },
+};
