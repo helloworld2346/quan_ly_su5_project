c1d192a31149bb093ff4e773ef86c5693c4f6ff7
GiaKhuong32 <khuonghoanggia3202@gmail.com>
2026-07-26 16:07:32 +0700

    chinh do rong bang

--- src/pages/DailyReport/DailyTroopReport.tsx
+++ src/pages/DailyReport/DailyTroopReport.tsx
@@ -947,7 +947,7 @@ const handleEditReport = (row: ReportRow) => {
                     await dailyReportService.createNhiemVuNgay(nhiemVuPayload);
                   }
                 } catch {
-                  // Không block nếu nhiemvungay fail
+                
                 }
               }
 
